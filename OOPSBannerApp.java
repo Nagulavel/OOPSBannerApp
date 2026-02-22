0a580d4f2aa812667eefbd1a322a6763015d9698
Nagulavel <Nagulavel01@gmail.com>
2026-02-22 18:48:00 +0530

    feature/UC2: Render OOPS as banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,63 @@
 /**
  * OOPSBannerApp
- * Prints OOPS to the console
+ * UC2: Render OOPS as Banner using ASCII Art
  *
  * @author Nagulavel
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String O1, O2, P, S;
+
+        // Line 1
+        O1 = "  *****  ";
+        O2 = "  *****  ";
+        P  = " ******  ";
+        S  = " *****   ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
+
+        // Line 2
+        O1 = " *     * ";
+        O2 = " *     * ";
+        P  = " *     * ";
+        S  = " *       ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
+
+        // Line 3
+        O1 = " *     * ";
+        O2 = " *     * ";
+        P  = " *     * ";
+        S  = " *       ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
+
+        // Line 4
+        O1 = " *     * ";
+        O2 = " *     * ";
+        P  = " ******  ";
+        S  = "  *****  ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
+
+        // Line 5
+        O1 = " *     * ";
+        O2 = " *     * ";
+        P  = " *       ";
+        S  = "       * ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
+
+        // Line 6
+        O1 = " *     * ";
+        O2 = " *     * ";
+        P  = " *       ";
+        S  = "       * ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
+
+        // Line 7
+        O1 = "  *****  ";
+        O2 = "  *****  ";
+        P  = " *       ";
+        S  = " *****   ";
+        System.out.println(O1 + " " + O2 + " " + P + " " + S);
     }
 }
